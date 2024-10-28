1d86d0ca12b1193cde6adbf91072f4c6af92dfe2
Kwon Jimin <jimin23623@gmail.com>
2024-10-28 15:00:38 +0900

    feat: mqtt 받아오는 코드 변경

--- publish.js
+++ publish.js
@@ -3,10 +3,17 @@ const client = mqtt.connect("mqtt://test.mosquitto.org");
 
 setInterval(() => {
   client.publish("test", "MQTT CONNECTED!");
-}, 5000);
+}, 2000);
 
-client.subscribe("test");
-
-client.on("message", function (topic, message) {
-  console.log(`토픽:${topic.toString()}, 메세지:${message.toString()}`);
+client.on("connect", () => {
+  if (client.connected) {
+    //mqtt가 연결되어있는지 확인
+    console.log(client.connected);
+    //topic 구독
+    client.subscribe("test");
+    //구독해놓은 메시지가 들어오면 실행
+    client.on("message", (topic, message) => {
+      console.log(`topic: ${topic.toString()}, message: ${message.toString()}`);
+    });
+  }
 });
